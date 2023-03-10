afa0c1c388859596d07d3376da8ce9dab7fb0671
AJ Espinosa <espinosa.aaron45@gmail.com>
2023-03-11 00:44:57 +0800

    fix bulb glow

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,7 +17,7 @@ module.exports = {
         glow: "0 0 1000px 80px rgba(223,147,40,0.7)",
       },
       dropShadow: {
-        bulbGlow: "0 0 30px rgba(255,255,255,0.3)",
+        bulbGlow: "0 0 30px #df9328",
         accentGlow: '0 0 5px #df9328',
         whiteGlow: '0 0 5px #ffffff',
         hover: '0 3px 0 #df9328',
