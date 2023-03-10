12e172202bd67de63fca5ad5662efe53b5ec13dd
AJ Espinosa <espinosa.aaron45@gmail.com>
2023-03-10 14:37:27 +0800

    Setup file structures

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
+import { About, Hero, Partners, Services } from "./views"
 
 function App() {
 
   return (
-    <></>
+    <>
+      <Hero />
+      <About />
+      <Partners />
+      <Services />
+    </>
   )
 }
 
---
+++ src/views/index.jsx
@@ -0,0 +1,11 @@
+import About from "./About";
+import Hero from "./Hero";
+import Partners from "./Partners";
+import Services from "./Services";
+
+export {
+    About,
+    Hero,
+    Partners,
+    Services,
+}
